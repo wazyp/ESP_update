6ad4719191a2174aecc24103169d026b4cb0abb5
wazyp <75336953+wazyp@users.noreply.github.com>
2024-04-29 10:18:55 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const multer = require('multer');
+const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
 const { networkInterfaces } = require('os');
  
 const app = express();
@@ -9,6 +11,42 @@ const nets = networkInterfaces();
 // Server port
 const PORT = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Initialize Firebase Admin SDK
+const serviceAccount = require('./firebase/serviceAccountKey.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+    // Other Firebase configuration options if needed
+});
+
+// Register endpoint
+app.post('/register', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const userRecord = await admin.auth().createUser({
+            email,
+            password
+        });
+        res.status(200).json({ message: 'User registered successfully', uid: userRecord.uid });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Login endpoint
+app.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const userRecord = await admin.auth().getUserByEmail(email);
+        // Perform authentication
+        // Example: You can compare userRecord.passwordHash with hashed password stored in your database
+        res.status(200).json({ message: 'Login successful', uid: userRecord.uid });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
  
  const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -39,8 +77,33 @@ app.get('/firmware/httpUpdateNew.bin', (request, response) => {
         }
     });
     console.log('Your file has been downloaded '+downloadCounter+' times!')
-})
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/register.html');
+});
  
+// Define a function to generate a random weight between 0 and 2000 grams
+const getRandomWeight = () => Math.floor(Math.random() * (2001 - 100) + 100);
+
+// Define a function to generate a random temperature between 10 and 40 degrees Celsius
+const getRandomTemperature = () => Math.floor(Math.random() * (41 - 10) + 10);
+
+// Endpoint to get random weight and temperature
+app.get('/getData', (req, res) => {
+    const weight = getRandomWeight();
+    const temperature = getRandomTemperature();
+
+    res.json({
+        weight: weight,
+        temperature: temperature
+    });
+});
+
 app.listen(PORT, () => {
     const results = {}; // Or just '{}', an empty object
  
